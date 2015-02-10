92f93a775d59a6a818e29e62f2cda720d4875d8f
Julian Veling <julianveling@Julians-MacBook-Pro.local>
2015-02-10 19:48:29 +0000

    back to the original initialize method in Game module, which seems to allow the ships double to exist, wheras the ships method returns nil

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,12 +1,15 @@
 require 'board'
 
+class Game_holder; include Game; end
+
 describe Board do
 
 	let(:board){ Board.new }
 	let(:ship){ double :ship, position: 'A1'}
 
-	it 'will be able to store a ship in an array' do
-		expect{board.store_ships(ship)}.to change{board.ship_count}.from(0).to(1)
-	end
+	it 'lets ship be stored in an array' do
+    board.store_ships(ship)
+    expect(board.ship_count).to eq(1)    
+  end
 
 end
