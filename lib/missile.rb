446c129bb8d368101564760e8a59c1f9533a9d03
Julian Veling <julianveling@Julians-MacBook-Pro.local>
2015-02-10 16:42:02 +0000

    added missile capability to have a position

--- lib/missile.rb
+++ lib/missile.rb
@@ -1,15 +1,20 @@
 class Missile
 	
-  attr_reader :cell
+  attr_reader :cell, :position
 
   DEFAULT_CELL = 1
 
   def initialize(options={})
     @cell = options.fetch(:cell, DEFAULT_CELL)
+    @position = options.fetch(:position)
   end
 
   def cell_count(board)
   	@cell = board
   end
+
+  def position
+    @position
+  end  
 
 end  
--- spec/missile_spec.rb
+++ spec/missile_spec.rb
@@ -1,8 +1,9 @@
 require 'missile'
 
 describe 'Missile' do 
-  let(:missile){Missile.new}
+  let(:missile){Missile.new position:'A1'}
   let(:board){ double :board, cells: 4}
+  let(:ship){double :ship, position: 'A1'}
 
   it 'should be one cell' do
     expect(missile.cell).to eq(1)
@@ -11,5 +12,9 @@ describe 'Missile' do
   it 'should be equal to the amount of cells on the board' do
   	expect(missile.cell_count(board.cells)).to eq 4
   end
+
+  it 'needs to have a position' do
+    expect(missile.position).to eq('A1')
+  end  
   
 end  
