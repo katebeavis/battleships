6f19875dad4fb7485ac3ce1e828f7c6288f73118
Julian Veling <julianveling@Julians-MacBook-Pro.local>
2015-02-10 14:01:36 +0000

    added possible game initialization method

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,11 @@
 class Game
 	
+  def initialize(options={})
+    @player1 = Player.new(options.fetch(:player1))
+    @player2 = Player.new(options.fetch(:player2))
+    @board = Board.new(options.fetch(:board))
+  end  
+
   def relay_move(coordinates)
     board.relay_move(coordinates)
   end  
