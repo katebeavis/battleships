8202497d55d155239b74ac7d1abdec1524450e3f
Kate Beavis <katebeavis84@gmail.com>
2015-02-10 16:00:02 +0000

    Added position to ship class

--- lib/ship.rb
+++ lib/ship.rb
@@ -3,9 +3,11 @@ class Ship
   attr_accessor :cells, :position
 
   DEFAULT_CELLS = 2
+  DEFAULT_POSITION = 'A1'
 
   def initialize(options={})
     @cells = options.fetch(:cells, DEFAULT_CELLS)
+    @position = options.fetch(:position, DEFAULT_POSITION)
   end
 
 end  
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -5,6 +5,10 @@ describe 'Ship' do
 
   it 'should have default number of cells' do
     expect(ship.cells).to eq(2)
-  end  
+  end
+
+  it 'should have a position' do
+  	expect(ship.position).to eq('A1')
+  end
 
 end  
