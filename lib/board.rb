5958b21571395653d16b239e2e538e7fd213fd67
Kate Beavis <katebeavis84@gmail.com>
2015-02-12 12:35:23 +0000

    Added grid with keys to board class

--- lib/board.rb
+++ lib/board.rb
@@ -3,7 +3,20 @@ class Board
   attr_reader :grid
   
   def initialize
-    @grid = {:A1 => "", :A2 => "", :B1 => "", :B2 =>  ""}  
+    @grid = {}
+    number_array=[]
+    (1..2).each do |number|
+     number_array<<number.to_s
+    end
+
+    symbols=[]
+    ('A'..'B').each do |letter|
+     number_array.each do|number|
+       symbols << letter+number
+     end
+    end 
+    symbols.map!{|symbol| symbol.to_sym}
+    symbols.each{|symbol| grid[symbol]=""}
   end
 
   def grid_length
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,6 +1,5 @@
 require 'board'
 
-
 describe Board do
 
 	let(:board){ Board.new }
@@ -9,5 +8,9 @@ describe Board do
   it 'can return the length of the grid' do
     expect(board.grid_length).to eq(4) 
   end
+
+  it 'should have a symbol as a key' do
+    board.grid.each {|k,v| expect(k).to be_a Symbol}
+	end
 
 end
